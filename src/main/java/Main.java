760c54a05f3f5f26fc200f7eaeb5f03f674bea7a
gusteran <ateran@wpi.edu>
2020-01-18 17:14:38 -0500

    Added my name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] teamT){
+        System.out.println("Hello team T!");
+        System.out.println("This is Gus");
+    }
 }
